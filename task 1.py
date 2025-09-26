5c8a69af7499d529130d11abd6f1a0e89b83afc9
DHIVYA SV <vtu26230@veltech.edu.in>
2025-09-25 23:51:18 -1200

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,20 @@
+graph = {
+  '1' : ['2','3'],
+  '2' : ['2', '5'],
+  '3' : ['6'],
+  '4' : [],
+  '5' : ['6'],
+  '6' : []
+}
+
+visited = set() 
+
+def dfs(visited, graph, node): 
+    if node not in visited:
+        print (node)
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+
+print("Following is the Depth-First Search")
+dfs(visited, graph, '1')
