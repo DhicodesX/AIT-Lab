cfd534d39385a855d73ff05f2b17455f29a2d5c8
DHIVYA SV <vtu26230@veltech.edu.in>
2025-09-25 23:58:02 -1200

    Add files via upload

---
+++ task 1b.py
@@ -0,0 +1,21 @@
+graph = {
+  '1' : ['2','3'],
+  '2' : ['2', '5'],
+  '3' : ['6'],
+  '4' : [],
+  '5' : ['6'],
+  '6' : []
+}
+
+visited = set() # Set to keep track of visited nodes of graph.
+
+def dfs(visited, graph, node):  #function for dfs 
+    if node not in visited:
+        print (node)
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+
+# Driver Code
+print("Following is the Depth-First Search")
+dfs(visited, graph, '1')
